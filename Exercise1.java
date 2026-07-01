b78c716fd9cb6229da5355f69f50c8983d458180
Kelley Jenkins <kelley.jenkins2456@gmail.com>
2026-07-01 13:17:47 -0500

    Initial Commit

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+public class Exercise1{
+    public static void main(String[] args) {
+        int numberOfTickets = input.nextInt();
+        
+        int ticketPrice = 15;
+
+        int TotalCost = ticketPrice * numberOfTickets;
+
+        System.out.println("Total Cost:$ " + TotalCost);
+        System.out.println("Enjoy the event");
+    }
+}
